c73610396fb7c24c5664926004a6213aa53fdac7
Alejandro Mendoza <ayauri190106@gmail.com>
2025-09-25 17:22:53 -0500

    primercommit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TF_HospitalDeApoyoDeCoraCora.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
